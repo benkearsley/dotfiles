aa1e429fecc918fe153290bde0d5f45804f07639
Ben Kearsley <ben.kearsley@outlook.com>
2025-06-07 12:37:03 -0500

    remove debugging

--- sketchybar/start_sketchybar.sh
+++ sketchybar/start_sketchybar.sh
@@ -4,8 +4,5 @@
 export XDG_CONFIG_HOME="$HOME/.config"
 CONFIG_DIR="$XDG_CONFIG_HOME/sketchybar"
 
-# Debug output
-echo "DEBUG start_sketchybar.sh: Starting sketchybar with XDG_CONFIG_HOME=$XDG_CONFIG_HOME" >> /tmp/sketchybar_debug.log
-
 # Start sketchybar with the config (theme will be sourced in sketchybarrc)
 exec /opt/homebrew/bin/sketchybar --config "$CONFIG_DIR/sketchybarrc" 
