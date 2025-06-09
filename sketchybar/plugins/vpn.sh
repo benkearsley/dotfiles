86f89d9b92670b3c607192af234da7a3f9bd010c
Ben Kearsley <ben.kearsley@outlook.com>
2025-06-08 21:41:22 -0500

    wip working

---
+++ sketchybar/plugins/vpn.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check for VPN connection using scutil
+VPN=$(scutil --nc list | grep Connected | sed -E 's/.*"(.*)".*/\1/')
+
+if [[ -n "$VPN" ]]; then
+  # VPN is connected - show with green color and connection name
+  sketchybar -m --set vpn icon="🔒" \
+                          label="$VPN" \
+                          icon.color=0xff8bd5ca \
+                          label.color=0xff8bd5ca \
+                          drawing=on
+else
+  # No VPN connection - show disconnected state
+  sketchybar -m --set vpn icon="🔓" \
+                          label="Disconnected" \
+                          icon.color=0xfff5a97f \
+                          label.color=0xfff5a97f \
+                          drawing=on
+fi
